1ea1961cfc790caba50f733d5e50f0304a8fb9cd
kaylaboch <boch.kayla57@gmail.com>
2016-04-15 16:19:04 -0400

    bounce

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 var $drinks= $('.drinks');
+var $people= $('.people');
 
 $drinks.waypoint(function (direction) {
   if (direction == 'down') {
     $drinks.addClass('js-drinks-animate');
   } else {
     $drinks.removeClass('js-drinks-animate');
+  }
+}, { offset: '50%' });
+
+$people.waypoint(function (direction) {
+  if (direction == 'down') {
+    $people.addClass('js-people-animate');
+  } else {
+    $people.removeClass('js-people-animate');
   }
 }, { offset: '50%' });
