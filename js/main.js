04f5800518dde3989ad0c4c7c84db0e4c06ab467
kaylaboch <boch.kayla57@gmail.com>
2016-04-15 15:40:20 -0400

    fixed spelling

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var $drinks= ('.drinks');
+
+$drinks.waypoint(function (direction) {
+  if (direction == 'down') {
+    $drinks.addClass('js-drinks-animate');
+  } else {
+    $drinks.removeClass('js-drinks-animate');
+  }, { offset: '50%' });
